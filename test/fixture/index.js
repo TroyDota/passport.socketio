6b6b4f77a36027fd708bef5c5c059e960fe70056
troydota <troybensonsa@gmail.com>
2020-08-12 18:59:46 +0200

    Fixes

--- test/fixture/index.js
+++ test/fixture/index.js
@@ -1,59 +1,63 @@
-var express = require('express');
-var connect   = require('connect');
-var passport  = require('passport');
+var express = require("express");
+var connect = require("connect");
+var passport = require("passport");
 
-var http = require('http');
-var xtend = require('xtend');
+var http = require("http");
+var xtend = require("xtend");
 
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
-var socketIo = require('socket.io'),
-    passportSocketIo = require('../../lib');
+var socketIo = require("socket.io"),
+  passportSocketIo = require("../../lib");
 
-var sessionStore    = new connect.session.MemoryStore(),
-    sessionSecret  = 'asdasdsdas1312312',
-    sessionKey    = 'test-session-key',
-    sessionOptions = {
-      store:  sessionStore,
-      key:    sessionKey,
-      secret: sessionSecret,
-      saveUninitialized: true,
-      resave: true
-    };
+var sessionStore = new connect.session.MemoryStore(),
+  sessionSecret = "asdasdsdas1312312",
+  sessionKey = "test-session-key",
+  sessionOptions = {
+    store: sessionStore,
+    key: sessionKey,
+    secret: sessionSecret,
+    saveUninitialized: true,
+    resave: true,
+  };
 
 var server;
 
-require('./setupPassport');
+require("./setupPassport");
 
 exports.start = function (options, callback) {
-
-  if(typeof options == 'function'){
+  if (typeof options == "function") {
     callback = options;
     options = {};
   }
 
   var app = express();
 
-  (function(){
+  (function () {
     app.use(cookieParser());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use(session(sessionOptions));
 
     app.use(passport.initialize());
     app.use(passport.session());
-  }).call(app);
+  }.call(app));
 
-  app.post('/login', passport.authenticate('local', { successRedirect: '/',
-                                                      failureRedirect: '/login',
-                                                      failureFlash: true }));
+  app.post(
+    "/login",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true,
+    })
+  );
 
-  app.get('/', function(req, res){
-    if(!req.user){
+  app.get("/", function (req, res) {
+    if (!req.user) {
       res.send(401);
-    }else{
+    } else {
       res.json(req.user);
     }
   });
@@ -61,19 +65,20 @@ exports.start = function (options, callback) {
   server = http.createServer(app);
 
   var sio = socketIo.listen(server);
-  sio.configure(function(){
-    var authorization = passportSocketIo.authorize(xtend(sessionOptions, options, {
-      cookieParser: cookieParser
-    }));
+  sio.configure(function () {
+    var authorization = passportSocketIo.authorize(
+      xtend(sessionOptions, options, {
+        cookieParser: cookieParser,
+      })
+    );
 
-    this.set('authorization', authorization);
+    this.set("authorization", authorization);
 
-    this.set('log level', 0);
-
+    this.set("log level", 0);
   });
 
-  sio.sockets.on('echo', function (m) {
-    sio.sockets.emit('echo-response', m);
+  sio.sockets.on("echo", function (m) {
+    sio.sockets.emit("echo-response", m);
   });
 
   server.listen(9000, callback);
