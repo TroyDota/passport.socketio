1e77a5b2f5d53205a7fe86f7b1dc8e7889ca106b
troydota <troybensonsa@gmail.com>
2020-08-12 18:00:52 +0200

    Session Keys

--- test/fixture/setSocketIOHandshakeCookies.js
+++ test/fixture/setSocketIOHandshakeCookies.js
@@ -1,20 +1,22 @@
-var xmlhttprequest = require('xmlhttprequest');
+var xmlhttprequest = require("xmlhttprequest");
 var originalRequest = xmlhttprequest.XMLHttpRequest;
 
 module.exports = function (jar) {
-  xmlhttprequest.XMLHttpRequest = function(){
+  xmlhttprequest.XMLHttpRequest = function () {
     originalRequest.apply(this, arguments);
     this.setDisableHeaderCheck(true);
-    
+
     var stdOpen = this.open;
 
-    this.open = function() {
+    this.open = function () {
       stdOpen.apply(this, arguments);
-      var header = jar.get({ url: 'http://localhost:9000' })
+      var header = jar
+        .get({ url: "http://localhost:9000" })
         .map(function (c) {
           return c.name + "=" + c.value;
-        }).join("; ");
-      this.setRequestHeader('cookie', header);
+        })
+        .join("; ");
+      this.setRequestHeader("cookie", header);
     };
   };
 };
